9f25504aab4f3c628dae0ad514ec44787c3e5ecc
Charlie Willoughby <104572309+CWilloughby98@users.noreply.github.com>
2024-04-04 03:42:02 +0000

    finished-project

--- src/app.js
+++ src/app.js
@@ -2,21 +2,17 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-// Creamos variables con el contenido de la carta
-const naipe = ["♥", "♣", "♦", "♠"];
+const suit = ["♥", "♣", "♦", "♠"];
 const numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
 function generateRandomCard() {
-  const suit = naipe[Math.floor(Math.random() * naipe.length)];
-  const number = numbers[Math.floor(Math.random() * numbers.length)];
-  const suitColor = suit === "♥" || suit === "♦" ? "text-danger" : "text-black";
+  const randomSuit = suit[Math.floor(Math.random() * suit.length)];
+  const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  const suitColor = randomSuit === "♥" || randomSuit === "♦" ? "text-danger" : "text-black";
   return `
-    <div class="card-font align-self-start ${suitColor}">${suit}</div>
-    <div class="card-font fw-bold">${number}</div>
-    <div class="rotated card-font align-self-end ${suitColor}">${suit}</div>
+    <div class=" card-font align-self-start ${suitColor}">${randomSuit}</div>
+    <div class="card-font fw-bold">${randomNumber}</div>
+    <div class="rotated card-font align-self-end ${suitColor}">${randomSuit}</div>
   `;
 }
 
@@ -24,9 +20,11 @@ function generateCard() {
   const card = document.getElementById("card");
   card.innerHTML = generateRandomCard();
 }
+
 //Button to generate a card
 const button = document.getElementById("button");
 button.addEventListener("click", generateCard);
+
 //Timer of 10seconds to generate card
 let countStart = 10;
 const counter = document.getElementById("counter");
@@ -39,5 +37,31 @@ const countDown = setInterval(() => {
     countStart = 10;
   }
 }, 1000);
+
+//Function to adjust card size
+function adjustSize() {
+  const inputW = document.getElementById("inputW");
+  const inputH = document.getElementById("inputH");
+  const card = document.getElementById("card");
+
+  // Get sizes from the inputs
+  const selectedWidth = inputW.value;
+  const selectedHeight = inputH.value;
+
+  const parsedWidth = parseInt(selectedWidth, 10);
+  const parsedHeight = parseInt(selectedHeight, 10);
+
+  const validWidth = Math.max(150, parsedWidth);
+  const validHeight = Math.max(214, parsedHeight);
+
+  card.style.width = validWidth + "px";
+  card.style.height = validHeight + "px";
+}
+
+const inputW = document.getElementById("inputW");
+inputW.addEventListener("input", adjustSize);
+
+const inputH = document.getElementById("inputH");
+inputH.addEventListener("input", adjustSize);
 
 window.onload = generateCard();
